8d6fac1d016672d84cbc094fff66d199aa7536e0
ozgeguner <ozgecaylioglu@gmail.com>
2020-09-23 17:37:58 +0000

    add client

---
+++ client.js
@@ -0,0 +1,22 @@
+const net = require('net');
+
+const connect = function() {
+  const conn = net.createConnection({ 
+    host: "135.23.222.131",
+    port: 50542,
+    
+  });
+  // interpret incoming data as text
+  // conn.setEncoding('utf8'); 
+  conn.on('connect', () => {
+    conn.write("Name: ozge")
+  });
+  conn.setEncoding('utf8'); // interpret data as text
+  conn.on('data', (data) => {
+    console.log('Server says: ', data)
+  });
+  return conn;
+}
+
+  
+module.exports = {connect};
